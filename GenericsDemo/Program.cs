df77cd3122188e4aed46845486c4f2cedd9c03b1
patt26 <kharchepratik123@gmail.com>
2021-05-04 19:41:53 +0530

    Added Regular Method for Generics

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H','E','L','L','O' };
+
+            Program.toPrint(inArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+            Console.ReadLine();
+
+        }
+
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------");
+        }   
+        public static void toPrint(double[] inputArray)
+        {
+            foreach  (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------");
+        }
+        
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------");
+        }
+
+
+    }
+}
