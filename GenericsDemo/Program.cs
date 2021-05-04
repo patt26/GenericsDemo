a9af9eaafdf1b0933a1cec2ef29717cc13bf8424
patt26 <kharchepratik123@gmail.com>
2021-05-04 19:49:47 +0530

    Added Generics Method

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -14,38 +14,61 @@ namespace GenericsDemo
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArray = { 'H','E','L','L','O' };
 
-            Program.toPrint(inArray);
-            Program.toPrint(doubleArray);
-            Program.toPrint(charArray);
+            Program.toPrint<int>(inArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+
+
+
+
+            //Program.toPrint(inArray);
+            //Program.toPrint(doubleArray);
+            //Program.toPrint(charArray);
             Console.ReadLine();
 
         }
 
-        public static void toPrint(int[] inputArray)
+        public static void toPrint<T>(T[] inputArray)
         {
-            foreach (int element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("-------------------");
-        }   
-        public static void toPrint(double[] inputArray)
-        {
-            foreach  (double element in inputArray)
+            foreach (var element in inputArray)
             {
                 Console.WriteLine(element);
             }
             Console.WriteLine("-------------------");
         }
+
+
+
+
+
+
+
+
+        //public static void toPrint(int[] inputArray)
+        //{
+        //    foreach (int element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------");
+        //}   
+        //public static void toPrint(double[] inputArray)
+        //{
+        //    foreach  (double element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------");
+        //}
         
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("---------------------");
-        }
+        //public static void toPrint(char[] inputArray)
+        //{
+        //    foreach (char element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("---------------------");
+        //}
 
 
     }
